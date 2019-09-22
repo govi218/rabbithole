8413ec2c3e85f57b8d2de4ed00bbbf344ca339cc
root <root@DESKTOP-3T3GPJ3.localdomain>
2019-09-22 18:37:41 -0400

    Added Projects Grid View, with minimal styling

--- src/components/ProjectsView/ProjectsView.js
+++ src/components/ProjectsView/ProjectsView.js
@@ -2,35 +2,51 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
+import { CardContent } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
+    padding: 20,
+    alignContent: 'center'
   },
   card: {
-    height: 200,
-    width: 200,
+    minWidth: 225,
   },
   control: {
     padding: theme.spacing(2),
   },
 }));
 
-export default function SpacingGrid() {
+/**
+ * Functional Component to render Projects View.
+ */
+export default function ProjectsView() {
   const [spacing, setSpacing] = React.useState(2);
   const classes = useStyles();
+
 
   function handleChange(event) {
     setSpacing(Number(event.target.value));
   }
 
   return (
-    <Grid container className={classes.root} spacing={5}>
+    <Grid container justify="center" className={classes.root} spacing={10}>
       <Grid item xs={12}>
         <Grid container spacing={spacing}>
-          {[0, 1, 2].map(value => (
+          {[0, 1, 2, 3, 4].map(value => (
             <Grid key={value} item>
-              <Card className={classes.card} />
+              <Card className={classes.card}>
+                <CardContent>
+                  <Typography className={classes.title} color="textPrimary">
+                    Title
+                  </Typography>
+                  <Typography className={classes.title} color="textSecondary">
+                    Date Created
+                  </Typography>
+                </CardContent>
+              </Card>
             </Grid>
           ))}
         </Grid>
