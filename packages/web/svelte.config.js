3584c37afc6e262e8936b37400794057a4b86dea
gov <info@govindmohan.com>
2026-03-21 13:20:14 -0700

    chore: lift all monorepo stuff

--- packages/web/svelte.config.js
+++ packages/web/svelte.config.js
@@ -1,23 +1,23 @@
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			edge: true
-		}),
-		alias: {
-			"@rabbithole/shared/types": "../shared/src/utils/types.ts",
-			"@rabbithole/shared/atproto/http": "../shared/src/atproto/http.ts",
-			"@rabbithole/shared/atproto/explore": "../shared/src/atproto/explore.ts",
-			"@rabbithole/shared/lib": "../shared/src/lib",
-		}
-	}
+  kit: {
+    adapter: adapter({
+      fallback: "index.html",
+    }),
+    alias: {
+      "@rabbithole/shared/types": "../shared/src/utils/types.ts",
+      "@rabbithole/shared/atproto/http": "../shared/src/atproto/http.ts",
+      "@rabbithole/shared/atproto/explore": "../shared/src/atproto/explore.ts",
+      "@rabbithole/shared/lib": "../shared/src/lib",
+    },
+  },
 };
 
 export default config;
