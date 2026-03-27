5af25257358302525d07aac31894d5365198252b
gov <info@govindmohan.com>
2026-03-27 14:26:56 -0700

    fix: remove datax

--- packages/web/src/lib/Footer.svelte
+++ packages/web/src/lib/Footer.svelte
@@ -6,11 +6,6 @@
   class="max-w-4xl mx-auto w-full flex md:flex-row flex-col items-center md:justify-between py-8 mt-20 border-t border-white/20"
 >
   <div class="text-white/70 flex items-center flex-none">
-    Powered by <a href="https://github.com/dataxresearch"
-      ><span class="mx-1 font-bold"> DataX Research</span></a
-    >
-  </div>
-  <div class="text-white/70 flex items-center flex-none">
     <a href="/privacy.html"
       ><span class="mx-1 font-bold">Privacy policy</span></a
     >
